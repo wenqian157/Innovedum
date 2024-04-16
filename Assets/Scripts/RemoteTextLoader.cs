c0eb35cccf4adcf36667cd4cd3302c0c1d1d8b79
wenqian157 <44199787+wenqian157@users.noreply.github.com>
2024-04-16 09:56:33 +0200

    update

--- Assets/Scripts/RemoteTextLoader.cs
+++ Assets/Scripts/RemoteTextLoader.cs
@@ -1,17 +1,46 @@
- using System.Collections;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class RemoteTextLoader : MonoBehaviour
 {
-    public TextMeshProUGUI stepName;
+    public TEXDraw texDraw;
+    public string[] data;
     void Start()
     {
-        LoadText();
+        StartCoroutine(ReadText());
     }
-    private void LoadText()
+    public void OnUILoadText()
     {
-
+        StartCoroutine(ReadText());
+    }
+    public IEnumerator ReadText()
+    {
+        yield return new WaitForSeconds(1.0f);
+        using (UnityWebRequest www = UnityWebRequest.Get(RemoteCSVLoader.urlBase + "/md" + "/text.md"))
+        {
+            www.SendWebRequest();
+            if (!string.IsNullOrEmpty(www.error))
+            {
+                Debug.LogError($"{www.error}");
+                yield break;
+            }
+            while (!www.isDone)
+            {
+                Debug.Log("loading text...");
+                yield return new WaitForSeconds(0.2f);
+            }
+            string stringData = www.downloadHandler.text;
+            data = stringData.Split(new string[] {"===="}, StringSplitOptions.None);
+            UpdateText(0);
+        }
+    }
+    public void UpdateText(int state)
+    {
+        string currentText = data[state].Substring(4);
+        texDraw.text = currentText;
     }
 }
--- OnlineResources/text.md
+++
@@ -1,9 +0,0 @@
-**Tie A**
-
-$$F_{d,A} = 506 kN$$
-
-$$A_{s,erf,A} = F_{d,A}/f_{sd}=506kN/435kN=1164mm^2$$
-
-selected reinforcement: 4Ø20
-
-$$A_{s,A} = 4 \pi r^2 = 4\cdot\pi\cdot(10mm)^2=1257mm^2$$
