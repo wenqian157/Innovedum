9b247eaa822d591127f5d8019534de59254b8106
wenqian157 <yang@arch.ethz.ch>
2024-07-09 14:22:12 +0200

    add obj test

--- Assets/Scripts/TestObjLoader.cs
+++ Assets/Scripts/TestObjLoader.cs
@@ -12,7 +12,8 @@ public class TestObjLoader : MonoBehaviour
     void Start()
     {
         url = "https://raw.githubusercontent.com/wenqian157/Innovedum/main/OnlineResources/obj/mesh_concrete_beam.obj";
-        StartCoroutine(LoadObjAsync());
+        //StartCoroutine(LoadObjAsync());
+        StartCoroutine(LoadObjAsyncNewMethod());
     }
     IEnumerator LoadObjAsync()
     {
@@ -31,8 +32,43 @@ public class TestObjLoader : MonoBehaviour
             }
             var textStream = new MemoryStream(Encoding.UTF8.GetBytes(www.downloadHandler.text));
             GameObject mesh = new OBJLoader().Load(textStream);
+            mesh.name = "oldMethod";
             mesh.transform.SetParent(this.transform);
             Debug.Log(www.result);
+            Debug.Log($"old method loading position: {mesh.transform.position}");
+            Debug.Log($"old method loading scale: {mesh.transform.localScale}");
+        }
+    }
+    IEnumerator LoadObjAsyncNewMethod()
+    {
+        using (UnityWebRequest www = UnityWebRequest.Get(url))
+        {
+            www.SendWebRequest();
+            if (!string.IsNullOrEmpty(www.error))
+            {
+                Debug.LogError($"{www.error}");
+                yield break;
+            }
+            while (!www.isDone)
+            {
+                Debug.Log("loading...");
+                yield return new WaitForSeconds(0.2f);
+            }
+
+            GameObject meshGO = ObjReader.ObjToMeshObject(www.downloadHandler.text);
+
+            meshGO.name = "newMethod";
+            meshGO.transform.SetParent(this.transform);
+            //Debug.Log(www.result);
+            //Debug.Log($"new method loading position: {meshGO.transform.position}");
+            //Debug.Log($"new method loading transform: {meshGO.transform.localScale}");
+
+            Mesh mesh = meshGO.GetComponent<MeshFilter>().mesh;
+            Debug.Log($"found {mesh.vertexCount} vertices");
+            foreach (var item in mesh.vertices)
+            {
+                Debug.Log(item.x + " " + item.y+ " " + item.z);
+            }
         }
     }
 }
